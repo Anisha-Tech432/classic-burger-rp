7e1b1963c9e070fe7659d197aa7d356c37813ae1
Anisha Sharma <anishasharma.dev@gmail.com>
2026-02-09 20:23:20 +0530

    fixed white screen issue and updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/FoodDelivery-RP">
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
